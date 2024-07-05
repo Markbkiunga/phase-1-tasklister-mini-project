8fac8e82f4c99d6da39190ff8ff90b2cc3cd3ca4
Mark Brian <markbkiunga2017@gmail.com>
2024-07-05 21:49:19 +0300

    Close to completing Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target.new_task_description.value);
+  });
 });
+
+function buildToDo(todo) {
+  let p = document.createElement('p');
+  let button = document.createElement('button');
+  button.addEventListener('click', handleDelete);
+  button.textContent = 'X';
+  p.textContent = `${todo} `;
+  p.appendChild(button);
+  console.log(p);
+  document.querySelector('#list').appendChild('p');
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
